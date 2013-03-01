a57311ec0acacb8a046309768cc15954a4d4e5cd
if1live <4ipperz@gmail.com>
2013-03-01 15:02:38 +0900

    filesystem unittest 복붙

---
+++ haruna_test/sora/filesystem_test.cpp
@@ -0,0 +1,22 @@
+﻿// Ŭnicode please
+#include <gtest/gtest.h>
+#include "sora/filesystem.h"
+
+TEST(Filesystem, GetExtension) 
+{
+	using std::string;
+	using sora::Filesystem;
+
+	const char file1[] = "asdf.txt";
+	EXPECT_STREQ("txt", Filesystem::GetExtension(file1).c_str());
+	const char file2[] = "ad/sdf/asdf.txt";
+	EXPECT_STREQ("txt", Filesystem::GetExtension(file2).c_str());
+	const char file3[] = "ad\\sdf\\asdf.txt";
+	EXPECT_STREQ("txt", Filesystem::GetExtension(file3).c_str());
+	const char file4[] = "sdf";
+	EXPECT_STREQ("", Filesystem::GetExtension(file4).c_str());
+	const char file5[] = "ad/sd.f/asdf.txt";
+	EXPECT_STREQ("txt", Filesystem::GetExtension(file5).c_str());
+	const char file6[] = "ad/sd.f/asdf";
+	EXPECT_STREQ("", Filesystem::GetExtension(file6).c_str());
+}
---
+++ haruna_test/sora/low_level_c_file_test.cpp
@@ -0,0 +1,54 @@
+﻿// Ŭnicode please
+#include <gtest/gtest.h>
+#include "sora/low_level_c_file.h"
+#include "sora/memory_file.h"
+
+TEST(LowLevelCFile, test) 
+{
+	using sora::LowLevelCFile;
+	const char *path1 = "low_level_c_file.txt";
+
+	LowLevelCFile file1(path1);
+	EXPECT_EQ(false, file1.IsOpened());
+	ASSERT_EQ(true, file1.Open("rb"));
+
+	EXPECT_EQ(28, file1.GetLength());
+	const char *content = "this is line1.this is line2.";
+	const char *buffer = (const char *)file1.GetBuffer();
+	EXPECT_STREQ(content, buffer);
+}
+
+TEST(ReadonlyFile_WriteonlyFile, test) 
+{
+	using sora::WriteonlyCFile;
+	using sora::ReadonlyCFile;
+	const char path1[] = "asdf.txt";
+	const char content[] = "this is line1.this is line2.";
+
+	WriteonlyCFile file1(path1);
+	EXPECT_EQ(false, file1.IsOpened());
+	ASSERT_EQ(true, file1.Open());
+	file1.Write(content, sizeof(content));
+	file1.Close();
+
+	ReadonlyCFile file2(path1);
+	EXPECT_EQ(false, file2.IsOpened());
+	EXPECT_EQ(true, file2.Open());
+	EXPECT_EQ(sizeof(content), file2.GetLength());
+	const char *buffer = (const char *)file2.GetBuffer();
+	EXPECT_STREQ(content, buffer);
+}
+
+TEST(MemoryFile, test) 
+{
+	using sora::MemoryFile;
+	const char *path1 = "low_level_c_file.txt";
+
+	MemoryFile file1(path1);
+	EXPECT_EQ(false, file1.IsOpened());
+	ASSERT_EQ(true, file1.Open());
+
+	EXPECT_EQ(28, file1.GetLength());
+	const char *content = "this is line1.this is line2.";
+	EXPECT_STREQ(content, (const char*)file1.start);
+}
