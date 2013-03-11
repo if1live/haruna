443d7be1012585d1c8eef0c82295c7222f2c2749
if1live <libsora25@gmail.com>
2013-03-11 21:56:21 +0900

    간단한 문자열 라이브러리

--- haruna_test/sora/string_util_test.cpp
+++ haruna_test/sora/string_util_test.cpp
@@ -2,6 +2,11 @@
 #include "test_stdafx.h"
 #include "sora/string_util.h"
 
+using std::string;
+using std::wstring;
+using std::vector;
+using sora::StringUtil;
+
 TEST(StringTable, Get)
 {
 	sora::StringTable table;
@@ -14,4 +19,75 @@ TEST(StringTable, Get)
 	EXPECT_TRUE(a == b);
 	EXPECT_TRUE(a == c);
 	EXPECT_TRUE(a != d);
+}
+
+TEST(StringUtil, Trim) 
+{
+	string a("as df");
+	EXPECT_TRUE(StringUtil::Trim(a) == "as df");
+	string b("  as df");
+	EXPECT_TRUE(StringUtil::Trim(b) == "as df");
+	string c("as df  ");
+	EXPECT_TRUE(StringUtil::Trim(c) == "as df");
+	string d("  as df  ");
+	EXPECT_TRUE(StringUtil::Trim(d) == "as df");
+}
+
+TEST(StringUtil, LeftTrim) 
+{
+	string a("as df");
+	EXPECT_TRUE(StringUtil::LeftTrim(a) == "as df");
+	string b("  as df");
+	EXPECT_TRUE(StringUtil::LeftTrim(b) == "as df");
+	string c("as df  ");
+	EXPECT_TRUE(StringUtil::LeftTrim(c) == "as df  ");
+	string d("  as df  ");
+	EXPECT_TRUE(StringUtil::LeftTrim(d) == "as df  ");
+}
+
+TEST(StringUtil, RightTrim) 
+{
+	string a("as df");
+	EXPECT_TRUE(StringUtil::RightTrim(a) == "as df");
+	string b("  as df");
+	EXPECT_TRUE(StringUtil::RightTrim(b) == "  as df");
+	string c("as df  ");
+	EXPECT_TRUE(StringUtil::RightTrim(c) == "as df");
+	string d("  as df  ");
+	EXPECT_TRUE(StringUtil::RightTrim(d) == "  as df");
+}
+
+
+TEST(StringUtil, Split) 
+{
+	vector<string> result;
+	int retval;
+
+	//없는거 쪼개기 시도
+	string test0("kldp.org");
+	retval = sora::StringUtil::Split(test0, 'z', &result);
+	EXPECT_EQ(1, retval);
+
+	//1글자로 자르기
+	string test1("kldp.org/node/11");
+	result.clear();
+	retval = sora::StringUtil::Split(test1, '/', &result);
+	EXPECT_EQ(3, retval);
+	ASSERT_EQ(3, (int)result.size());
+	EXPECT_TRUE(result[0] == "kldp.org");
+	EXPECT_TRUE(result[1] == "node");
+	EXPECT_TRUE(result[2] == "11");	
+}
+
+TEST(StringUtil, Join) 
+{
+	string delim("::");
+	string result;
+
+	vector<string> strlist0;
+	strlist0.push_back("a");
+	strlist0.push_back("b");
+	strlist0.push_back("c");
+	result = StringUtil::Join(delim, strlist0);
+	EXPECT_TRUE(result == "a::b::c");
 }
--- sora/string_util.h
+++ sora/string_util.h
@@ -7,32 +7,113 @@
 namespace sora {;
 
 struct SID {
-    SID() : hash(0), str(nullptr), length(0) {}
-    SID(unsigned int hash, const char *str, int len)
-        : hash(hash), str(str), length(len) {}
-    unsigned int hash;
-    const char *str;
-    int length;
+	SID() : hash(0), str(nullptr), length(0) {}
+	SID(unsigned int hash, const char *str, int len)
+		: hash(hash), str(str), length(len) {}
+	unsigned int hash;
+	const char *str;
+	int length;
 
-    bool operator==(const SID &o) const;
-    bool operator!=(const SID &o) const;
+	bool operator==(const SID &o) const;
+	bool operator!=(const SID &o) const;
 };
 
 class StringTable {
-    typedef std::unordered_map<unsigned int, SID*> TableType;
-    typedef APHash HashFunc;
+	typedef std::unordered_map<unsigned int, SID*> TableType;
+	typedef APHash HashFunc;
 public:
-    StringTable();
-    ~StringTable();
+	StringTable();
+	~StringTable();
 
-    SID Get(const char *str);
-    SID Get(const char *str, int len);
-    SID Get(const std::string &str);
+	SID Get(const char *str);
+	SID Get(const char *str, int len);
+	SID Get(const std::string &str);
 
-    static StringTable &Global();
+	static StringTable &Global();
 private:
-    TableType table_;
+	TableType table_;
 };
 
+class StringUtil {
+public:
+	template<typename T>
+	static T Trim(const T &str) 
+	{
+		using std::size_t;
+
+		// do no use boost to compile speed + dependenty
+		const typename T::value_type *whitespace = "\t\n\x0b\x0c\r ";
+
+		size_t leftFound = str.find_first_not_of(whitespace);
+		size_t rightFound = str.find_last_not_of(whitespace);
+
+		if (leftFound == T::npos) {
+			leftFound = 0;
+		}
+		if (rightFound == T::npos) {
+			rightFound = str.length();
+		}
+		T result = str.substr(leftFound, rightFound-leftFound+1);
+		return result;
+	}
+
+	template<typename T>
+	static T LeftTrim(const T &str) 
+	{
+		using std::size_t;
+		size_t n = str.find_first_not_of("\t\n\x0b\x0c\r ");
+		T left(n == T::npos ? str : str.substr(n, str.length()));
+		return left;
+	}
+
+	template<typename T>
+	static T RightTrim(const T &str) 
+	{
+		using std::size_t;
+		size_t n = str.find_last_not_of("\t\n\x0b\x0c\r ");
+		T right(n == T::npos ? str : str.substr(0, n + 1));
+		return right;
+	}
+
+	template<typename T>
+	static T Join(const T &str, const std::vector<T> &tokenlist) 
+	{
+		std::ostringstream oss;
+		for (size_t i = 0 ; i < tokenlist.size() ; i++) {
+			oss << tokenlist[i];
+			if (i != tokenlist.size() - 1) {
+				oss << str;
+			}
+		}
+		return oss.str();
+	}
+
+	template<typename T>
+	static int Split(const T &str, typename T::value_type ch, std::vector<T> *retval) 
+	{
+		//SR_ASSERT(retval->empty() == true);
+		// if no delimeter
+		if (str.length() == 0) {
+			retval->push_back(str);
+			return retval->size();
+		}
+
+		// simple impl
+		T tmp_token;
+		for (size_t i = 0 ; i < str.length() ; i++) {
+			typename T::value_type str_ch = str[i];
+			if (str_ch == ch) {
+				// split point
+				retval->push_back(tmp_token);
+				tmp_token.clear();
+			} else {
+				tmp_token.push_back(str_ch);
+			}
+		}
+		retval->push_back(tmp_token);
+		return retval->size();
+	}
+
+};
 
 }   // namespace sora
