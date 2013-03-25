bbe8a9d2d9d13b40a6790ff2ffe24ffb2c19ce9d
if1live <libsora25@gmail.com>
2013-03-25 22:30:39 +0900

    glsl연습용 코드 삭제. 리펙토링에 걸리적거릴거같아서

--- haruna_exe/main.cpp
+++ haruna_exe/main.cpp
@@ -8,18 +8,6 @@
 #include <memory>
 #include "renderer.h"
 
-#include "color_shader.h"
-#include "texture_mapping.h"
-#include "lighting.h"
-#include "diffuse_specular_mapping.h"
-#include "toon_shader.h"
-#include "normal_mapping.h"
-#include "environment_mapping.h"
-#include "uv_animation.h"
-#include "color_conversion.h"
-#include "edge_detection.h"
-#include "shadow_mapping.h"
-
 #include "demo.h"
 
 #include "haruna/debug_draw_manager.h"
@@ -53,71 +41,9 @@ bool InitWindow(int width, int height)
 	return true;
 }
 
-std::unique_ptr<AbstractLogic> CreateLogicByMenu()
-{
-	printf("Select Example\n");
-	printf("1. Color Shader\n");
-	printf("2. Texture2D Mapping\n");
-	printf("3. Lighting\n");
-	printf("4. Diffuse Specular Mapping\n");
-	printf("5. ToonShader\n");
-	printf("6. Normal Mapping\n");
-	printf("7. Environment Mapping\n");
-	printf("8. UV Animation\n");
-	printf("9. Shadow Mapping\n");
-	printf("10. Color Conversion\n");
-	printf("11. Edge Detection\n");
-	int select = 0;
-	scanf("%d", &select);
-
-	std::unique_ptr<AbstractLogic> logic;
-	switch(select) {
-	case 1:
-		logic.reset(new ColorShader(kWidth, kHeight));
-		break;
-	case 2:
-		logic.reset(new Texture2DMapping(kWidth, kHeight));
-		break;
-	case 3:
-		logic.reset(new Lighting(kWidth, kHeight));
-		break;
-	case 4:
-		logic.reset(new DiffuseSpecularMapping(kWidth, kHeight));
-		break;
-	case 5:
-		logic.reset(new ToonShader(kWidth, kHeight));
-		break;
-	case 6:
-		logic.reset(new NormalMapping(kWidth, kHeight));
-		break;
-	case 7:
-		logic.reset(new EnvironmentMapping(kWidth, kHeight));
-		break;
-	case 8:
-		logic.reset(new UVAnimation(kWidth, kHeight));
-		break;
-	case 9:
-		logic.reset(new ShadowMapping(kWidth, kHeight));
-		break;
-	case 10:
-		logic.reset(new ColorConversion(kWidth, kHeight));
-		break;
-	case 11:
-		logic.reset(new EdgeDetection(kWidth, kHeight));
-		break;
-
-	default:
-		printf("Not support : %d\n", select);
-		getchar();
-		exit(EXIT_FAILURE);
-	}
-	return logic;
-}
-
 int main()
 {
 	//std::unique_ptr<AbstractLogic> logic = CreateLogicByMenu();
-	std::unique_ptr<AbstractLogic> logic(new Texture2DMapping(kWidth, kHeight));
 	//std::unique_ptr<AbstractLogic> logic(new NormalMapping(kWidth, kHeight));
 	//std::unique_ptr<AbstractLogic> logic(new DiffuseSpecularMapping(kWidth, kHeight));
 	//std::unique_ptr<AbstractLogic> logic(new EnvironmentMapping(kWidth, kHeight));
@@ -125,7 +51,7 @@ int main()
 	//std::unique_ptr<AbstractLogic> logic(new ColorConversion(kWidth, kHeight));
 	//std::unique_ptr<AbstractLogic> logic(new EdgeDetection(kWidth, kHeight));
 	//std::unique_ptr<AbstractLogic> logic(new ShadowMapping(kWidth, kHeight));
-	//std::unique_ptr<AbstractLogic> logic(new Demo(kWidth, kHeight));
+	std::unique_ptr<AbstractLogic> logic(new Demo(kWidth, kHeight));
 
 	InitWindow(static_cast<int>(kWidth), static_cast<int>(kHeight));
 
