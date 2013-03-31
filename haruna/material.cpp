b47af25fe11a6fc938d9de79a9475279407ddc39
if1live <libsora25@gmail.com>
2013-03-31 12:13:35 +0900

    material 복붙. 나중에 수정해서 쓰기

---
+++ haruna/material.cpp
@@ -0,0 +1,121 @@
+﻿// Ŭnicode please
+#include "stdafx.h"
+#include "material.h"
+
+namespace haruna {;
+
+//싱글턴처럼 사용하기 위한 용도
+MaterialManager g_mgr;
+
+bool Material_Init()
+{
+	g_mgr.Clear();
+	return true;
+}
+bool Material_Deinit()
+{
+	g_mgr.Clear();
+	return true;
+}
+
+Material::Material() 
+	: shininess(20.0f), 
+	props(0),
+	use_light(true),
+	type(kMaterialTypeUber) 
+{
+	//mtl format default value
+	for(int i = 0 ; i < 4 ; i++) {
+		ambient[i] = 1.0f;
+		diffuse[i] = 1.0f;
+		specular[i] = 1.0f;
+	}
+};
+
+const Material &Material::NullMaterial() 
+{
+	static Material mtl;
+	mtl.type = kMaterialTypeNone;
+	mtl.props = 0;  //0이면 플래그 검사 그냥 통과하니까 전부 disable의미
+	return mtl;
+}
+
+bool Material::operator==(const Material &o) const 
+{
+	if(type != o.type) { return false; }
+	if(use_light != o.use_light) { return false; }
+	if(name != o.name) { return false; }
+	if(props != o.props) { return false; }
+	if(ambient != o.ambient) { return false; }
+	if(diffuse != o.diffuse) { return false; }
+	if(specular != o.specular) { return false; }
+	if(shininess != o.shininess) { return false; }
+	if(diffuse_map != o.diffuse_map) { return false; }
+	if(specular_map != o.specular_map) { return false; }
+	if(normal_map != o.normal_map) { return false; }
+	return true;
+}
+
+bool Material::operator!=(const Material &o) const 
+{
+	return !(*this == o);
+}
+
+/////////////////////////////////////
+MaterialManager::MaterialManager() {}
+MaterialManager::~MaterialManager() {}
+
+MaterialManager &MaterialManager::GetInstance()
+{
+	return g_mgr;
+}
+
+bool MaterialManager::IsExist(const std::string &name) const 
+{
+	auto found = material_list_.find(name);
+	if(found == material_list_.end()) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+bool MaterialManager::Add(const std::vector<Material> &mtl_list) 
+{
+	auto it = mtl_list.begin();
+	auto endit = mtl_list.end();
+	bool collision_occur = false;
+	for( ; it != endit ; ++it) {
+		bool add_result = Add(*it);
+		if(add_result == false) {
+			collision_occur = true;
+		}
+	}
+	return collision_occur;
+}
+bool MaterialManager::Add(const Material &mtl) 
+{
+	if(IsExist(mtl.name) == true) {
+		//name collision
+		return false;
+	}
+	material_list_[mtl.name] = mtl;
+	return true;
+}
+const Material &MaterialManager::Get(const std::string &name) const 
+{
+	auto found = material_list_.find(name);
+	if(found == material_list_.end()) {
+		//else..not found, use basic material
+		static Material basic_material;
+		return basic_material;
+	}
+	return found->second;
+}
+void MaterialManager::Clear() 
+{
+	material_list_.clear();
+}
+
+} //namespace haruna
+
---
+++ haruna/material.h
@@ -0,0 +1,67 @@
+﻿// Ŭnicode please
+#pragma once
+
+namespace haruna {;
+typedef enum {
+	kMaterialTypeNone,
+	kMaterialTypeUber,
+} MaterialType;
+
+//material/uber shader에서 사용되는 플래그용
+enum {
+	kMaterialAmbient = 1 << 0,
+	kMaterialDiffuse = 1 << 1,
+	kMaterialSpecular = 1 << 2,
+	kMaterialDiffuseMap = 1 << 3,
+	kMaterialSpecularMap = 1 << 4,
+	kMaterialNormalMap = 1 << 5,
+};
+
+bool Material_Init();
+bool Material_Deinit();
+
+struct Material {
+	Material();
+	static const Material &NullMaterial();
+
+	MaterialType type;
+
+	std::string name;
+	unsigned int props;   //flag저장 용도
+	bool use_light; //빛이 없으면(false인 경우) diffuse_map를 albedo로써 사용한다
+
+	glm::vec4 ambient;
+	glm::vec4 diffuse;
+	glm::vec4 specular;
+	float shininess;  //specular
+
+	std::string diffuse_map;
+	std::string specular_map;
+	std::string normal_map;
+
+	bool operator==(const Material &o) const;
+	bool operator!=(const Material &o) const;
+};
+
+class MaterialManager {
+public:
+	MaterialManager();
+	~MaterialManager();
+
+	static MaterialManager &GetInstance();
+public:
+	//이름중복이 발생한 경우 false
+	bool Add(const std::vector<Material> &mtl_list);
+	bool Add(const Material &mtl);
+	bool IsExist(const std::string &name) const;
+	const Material &Get(const std::string &name) const;
+	void Clear();
+
+private:
+	//일단은 몇개 안될테니까 간단하게 구현
+	//재질정보는 그렇게 크지도 않고 많지도 않을테니까 전부 떄려박아도 심각한 문제가
+	//발생하지는 않을것이다.
+	typedef std::unordered_map<std::string, Material> MaterialDict;
+	MaterialDict material_list_;
+};
+} //namespace haruna
